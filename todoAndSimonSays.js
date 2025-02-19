5871f34985c38aaa7910dc275f13fe2c575b74d0
sachin2711-code <us038786@gmail.com>
2025-02-19 22:56:40 +0530

    second commit

--- todoAndSimonSays.js
+++ todoAndSimonSays.js
@@ -1,9 +1,9 @@
-// let inp=document.querySelector("input");
-// let btn=document.querySelector("button");
-// let ul=document.querySelector("ul");
-// btn.addEventListener("click",function(){
-//     let item=document.createElement("li");
-//     item.innerText=inp.value;
+let inp=document.querySelector("input");
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+btn.addEventListener("click",function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
    
     
 //     let btns=document.createElement("button");
